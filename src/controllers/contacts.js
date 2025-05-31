f1fa9c1bdd633bbd2036e28b78824efd1ccbf15b
Eugen <devilsbrother1@gmail.com>
2025-05-31 18:30:03 +0300

    done

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,8 +1,10 @@
 import createHttpError from 'http-errors';
 import {
   createContact,
+  deleteContact,
   getAllContacts,
   getContactById,
+  updateContact,
 } from '../services/contacts.js';
 
 export const getContactsController = async (req, res, next) => {
@@ -41,4 +43,32 @@ export const createContactController = async (req, res) => {
     message: 'Successfully created a contact',
     data: contact,
   });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
+
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(204).send();
 };
--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ export function setupServer() {
     });
   });
 
-  app.get(contactsRouter);
+  app.use(contactsRouter);
 
   app.use(notFoundHandler);
 
