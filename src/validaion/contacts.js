da9f1e41f102fef12beb3690f09cca27b55e6d8c
Eugen <devilsbrother1@gmail.com>
2025-06-20 18:59:42 +0300

    end

---
+++ src/validaion/contacts.js
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().length(12).pattern(/^\d+$/).required(),
+  email: Joi.string().email().allow(null),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('personal', 'home').required(),
+});
